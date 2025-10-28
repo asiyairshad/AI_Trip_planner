62cf390471404ce03c69a2e4c1244132b88ee291
Asiya Irshad <asiyairshad099@gmail.com>
2025-10-29 00:28:03 +0300

    code updated for loading the model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app=FastAPI()
 
-class QueryRequest(BaseaModel):
+class QueryRequest(BaseModel):
     query:str
 
 @app.post("/query")
@@ -21,3 +21,17 @@ async def query_travel_agent(query:QueryRequest):
             f.write(png_graph)
 
             print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+
+        # Assuming request is a pydantic object like: {"question": "your text"}
+        messages={"messages": [query.question]}
+        output = react_app.invoke(messages)
+
+        # If result is dict with messages:
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output["messages"][-1].content  # Last AI response
+        else:
+            final_output = str(output)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
